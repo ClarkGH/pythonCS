8662b51ce2468850a2a7015f8371461945edff9f
clarkgh <demolisho@hotmail.com>
2018-01-12 22:27:47 -0800

    recursion with python

--- recursiveSum.py
+++ recursiveSum.py
@@ -1,1 +1,9 @@
-print "hi mom"
+numberList = [1,2,3,4,5]
+
+def recursiveSum(numList):
+	if not numList:
+		return 0
+	else:
+		return numList[0] + recursiveSum(numList[1:])
+
+print recursiveSum( numberList )
